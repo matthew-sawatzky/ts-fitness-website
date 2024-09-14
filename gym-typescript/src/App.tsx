b9178643e0137350afa5054695afaaba73f7988a
MatthewSawatz <msawatzky2@rrc.ca>
2024-09-14 13:30:05 -0500

    nav bar added

--- gym-typescript/src/App.tsx
+++ gym-typescript/src/App.tsx
@@ -1,9 +1,14 @@
-
+import Navbar from "@/scenes/navbar/index";
+import { useState } from "react";
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState<string>("home");
+
   return (
     <>
-      <div className="app">app</div>
+      <div className="app bg-gray-20">
+        <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+      </div>
     </>
   );
 }
