3e243f17522663486ed6026c467cc34d88883ef0
MatthewSawatz <msawatzky2@rrc.ca>
2024-09-18 08:23:04 -0500

    mobile menu functionality added

--- gym-typescript/src/scenes/navbar/index.tsx
+++ gym-typescript/src/scenes/navbar/index.tsx
@@ -2,8 +2,9 @@ import Logo from "@/assets/Logo.png"
 import Link from "../navbar/Link"
 import { SelectedPage } from "@/shared/types"
 import useMediaQuery from "@/hooks/useMediaQuery";
-import { Bars3Icon } from "@heroicons/react/24/solid";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import { useState } from "react";
+import ActionButton from "@/shared/ActionButton";
 
 type Props = {
     selectedPage: SelectedPage;
@@ -43,7 +44,7 @@ setSelectedPage={setSelectedPage}/>
 </div>
 <div className={`${flexBetween} gap-8`}>
     <p>Sign In</p>
-    <button>Become a Member</button>
+    <ActionButton setSelectedPage={setSelectedPage}>Become a Member</ActionButton>
 </div>
 </div>)  : (
   <button className="rounded-full bg-secondary-500 p-2"
@@ -52,9 +53,20 @@ setSelectedPage={setSelectedPage}/>
 <Bars3Icon className="h-6 w-6 text-white"/>
   </button>
 )}
-</div>
         </div>
         </div>
+        </div>
+        {!isAboveMediumScreens && isMenuToggled && (
+          <div className="fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl">
+          <div className="flex justify-end p-12">
+            <button className="rounded-full bg-secondary-500 p-2"
+            onClick={() => setIsMenuToggled(!isMenuToggled)}
+            >
+              <XMarkIcon className="h-6 w-6 text-gray-400"/>
+            </button>
+          </div>
+          </div>
+        )}
     </nav>
   )
 }
